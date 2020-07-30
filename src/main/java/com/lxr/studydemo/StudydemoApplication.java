3cf526963cf7e202f44ec00e55c429fefb2f9844
liuxiangran <liuxr@strongflc.com>
2020-07-30 18:57:41 +0800

    20200730-lenovo

--- src/main/java/com/lxr/studydemo/StudydemoApplication.java
+++ src/main/java/com/lxr/studydemo/StudydemoApplication.java
@@ -1,9 +1,11 @@
 package com.lxr.studydemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lxr.studydemo.dao")
 public class StudydemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lxr/studydemo/service/MutiThreadService.java
+++ src/main/java/com/lxr/studydemo/service/MutiThreadService.java
@@ -1,25 +1,47 @@
 package com.lxr.studydemo.service;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class MutiThreadService {
 
-    @Autowired
-    private HelloWorldService helloService;
+//    @Autowired
+    private HelloWorldService helloService = new HelloWorldService();
 
-    ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
+    private static final int MAX_THREADS = 100;
+    private CountDownLatch cdl = new CountDownLatch(MAX_THREADS);
 
     @Test
+    public void testInterface() {
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    cdl.countDown();
+                    cdl.await(); //等待
+                    String str = helloService.helloWorld();
+                    System.out.println("====== " + str + "======");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            t.start();
+        }
+    }
+
+    @Test
+    @Ignore
     public void testThreadPool() {
 
-
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
         executor.scheduleAtFixedRate(() -> {
             helloService.helloWorld("liuxiangran");
         }, 0, 10, TimeUnit.MILLISECONDS);
