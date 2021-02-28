7eb39cddaa931807b402d0953a8735afd2ee2582
Areogel666 <32694454+Areogel666@users.noreply.github.com>
2021-02-28 23:56:40 +0800

    update

---
+++ src/test/java/com/lxr/studydemo/algorithm/Common.java
@@ -0,0 +1,44 @@
+package com.lxr.studydemo.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+public class Common {
+
+    /**
+     * 剑指 Offer 04. 二维数组中的查找
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        //暴力；二分法；binaryTree(左下或右上做顶点)
+        //以右上做顶点，大于15下移，小于15左移，以此类推 时间复杂度O(m+n)
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int left = m - 1;
+        while (top >= n || left < 0) {
+            int current = matrix[top][left];
+            if (target == current) {
+                return true;
+            } else if (target > current) {
+                top++;
+            } else if (target < current) {
+                left--;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void testFindNumberIn2DArray() {
+        int[][] matrix = {{1,   4,  7, 11, 15}
+                         ,{2,   5,  8, 12, 19}
+                         ,{3,   6,  9, 16, 22}
+                         ,{10, 13, 14, 17, 24}
+                         ,{18, 21, 23, 26, 30}};
+        boolean numberIn2DArray = findNumberIn2DArray(matrix, 87);
+        System.out.println(numberIn2DArray);
+    }
+}
--- src/test/java/com/lxr/studydemo/algorithm/Easy.java
+++ src/test/java/com/lxr/studydemo/algorithm/Easy.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class Easy {
 
@@ -22,6 +23,7 @@ public class Easy {
      * @return
      */
     public int[] twoSum(int[] nums, int target) {
+        //考虑采用哈希map查询，时间复杂度为O(1)，循环n次
         Map<Integer, Integer> numMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (numMap.containsKey(target - nums[i])) {
@@ -38,4 +40,68 @@ public class Easy {
         int target = 9;
         Arrays.stream(twoSum(nums, target)).forEach(System.out::println);
     }
+
+    /**
+     * 7. 整数反转
+     * 给你一个 32 位的有符号整数 x ，返回 x 中每位上的数字反转后的结果。
+     *
+     * 如果反转后整数超过 32 位的有符号整数的范围[−231, 231− 1] ，就返回 0。
+     *
+     * 假设环境不允许存储 64 位整数（有符号或无符号）。
+     *
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        boolean negative = false;
+        if (x < 0) {
+            negative = true;
+            x = -x;
+        }
+        String resStr = "";
+        String numStr = String.valueOf(x);
+        Stack<Character> numStack = new Stack<>();
+        for (int i = 0; i < numStr.length(); i++) {
+            numStack.push(numStr.charAt(i));
+        }
+        while (!numStack.empty()) {
+            Character c = numStack.pop();
+            resStr += c;
+        }
+        int resNum;
+        try {
+            resNum = Integer.parseInt(resStr);
+        } catch (NumberFormatException e) {
+            resNum = 0;
+        }
+        if (negative) {
+            return -resNum;
+        }
+        return resNum;
+    }
+
+    public int reverseBetter(int x) {
+        //考虑用数学方式，循环求余。同时每次判断res * 10 + x % 10是否会超限
+        int res = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && pop > Integer.MAX_VALUE % 10)){
+                return 0;
+            }
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && pop < Integer.MIN_VALUE % 10)){
+                return 0;
+            }
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        return res;
+    }
+
+    @Test
+    public void testReverse() {
+        System.out.println(reverse(1534236469));
+        System.out.println(reverseBetter(1534236469));
+    }
+
+
 }
