77221acba8fe350b369c83dc7c0f2f3897a3fbb2
刘相然 <liuxr@strongflc.com>
2021-03-01 18:15:35 +0800

    update

--- src/test/java/com/lxr/studydemo/algorithm/Easy.java
+++ src/test/java/com/lxr/studydemo/algorithm/Easy.java
@@ -2,10 +2,7 @@ package com.lxr.studydemo.algorithm;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class Easy {
 
@@ -103,5 +100,78 @@ public class Easy {
         System.out.println(reverseBetter(1534236469));
     }
 
+    /**
+     * 9. 回文数
+     *
+     * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+     *
+     * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+     *
+     * @param x
+     * @return boolean
+     */
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x == 0) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        while (x != 0) {
+            list.add(x % 10);
+            x /= 10;
+        }
+        int right;
+        int left;
+        for (int j = 0; j <= list.size() / 2; j++) {
+            right = list.get(j);
+            left = list.get(list.size() - j - 1);
+            if (right != left) return false;
+        }
+        return true;
+    }
 
+    public boolean isPalindrome1(int x) {
+        //数学方式
+        if (x < 0) return false;
+        int div = 1;
+        while (x / div >= 10) div *= 10; //求出最大除数
+        while (x > 0) {
+            int left = x / div;//左起
+            int right = x % 10;//右起
+            if (left != right) return false;
+            x = (x % div) / 10; //去除左右已比较数字
+            div /= 100; //由于去除两位，除数减少两位 100
+        }
+        return true;
+    }
+
+    public boolean isPalindrome2(int x) {
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10; //反转数
+            x /= 10;
+        }
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
+
+    @Test
+    public void testIsPalindrome() {
+        System.out.println(isPalindrome(1432341));
+        System.out.println(isPalindrome1(1432341));
+        System.out.println(isPalindrome2(1432341));
+    }
 }
