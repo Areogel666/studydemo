dbd6ac598010181484159a8f76aa680b941baff6
Areogel666 <32694454+Areogel666@users.noreply.github.com>
2021-03-21 14:30:02 +0800

    链表

--- src/test/java/com/lxr/studydemo/algorithm/easy/LinkListOp.java
+++ src/test/java/com/lxr/studydemo/algorithm/easy/LinkListOp.java
@@ -149,4 +149,27 @@ public class LinkListOp {
         node4.next = node2;
         System.out.println(hasCycle2(node1));
     }
+
+    /**
+     * 21. 合并两个有序链表
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //递归
+        //终止条件：当两个链表都为空时，表示我们对链表已合并完成。
+        //如何递归：我们判断 l1 和 l2 头结点哪个更小，然后较小结点的 next 指针指向其余结点的合并结果。（调用递归）
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        //如果l1小 合并l1，l1 = l1.next
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
