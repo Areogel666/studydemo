de068e96ae7cf3ffba4eb30df79797c7b2b0879e
Areogel666 <32694454+Areogel666@users.noreply.github.com>
2021-04-21 23:19:58 +0800

    update

--- src/test/java/com/lxr/studydemo/algorithm/easy/ArrayAndSort.java
+++ src/test/java/com/lxr/studydemo/algorithm/easy/ArrayAndSort.java
@@ -2,7 +2,7 @@ package com.lxr.studydemo.algorithm.easy;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -317,5 +317,45 @@ public class ArrayAndSort {
             s[i] = s[s.length - i - 1];
             s[s.length - i - 1] = temp;
         }
+    }
+
+    /**
+     * 496. 下一个更大元素 I
+     * 给你两个 没有重复元素 的数组nums1 和nums2，其中nums1是nums2的子集。
+     *
+     * 请你找出 nums1中每个元素在nums2中的下一个比其大的值。
+     *
+     * nums1中数字x的下一个更大元素是指x在nums2中对应位置的右边的第一个比x大的元素。如果不存在，对应位置输出 -1 。
+     *
+     * 示例 1:
+     *
+     * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+     * 输出: [-1,3,-1]
+     * 解释:
+     *     对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+     *     对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+     *     对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // 使用单调栈（栈内的元素都保持有序）
+        // 首先遍历nums2，建立单调栈，每次弹出栈得到下一个更大数字后存入哈希集合，最后根据num1取出结果
+        Deque<Integer> stack = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            // 建立单调栈
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) { //栈中上一个元素比当前元素小，则记录上一个元素的下一更大元素为nums2[i]
+                map.put(stack.pop(), nums2[i]); //pop掉栈中更小元素，因此栈是有序的（后入栈元素总是更大）
+            }
+            stack.push(nums2[i]);
+        }
+        //根据map集合查出结果
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
     }
 }
