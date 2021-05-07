0a83bda7faac5301b257181095706bed0269827b
刘相然 <liuxr@strongflc.com>
2021-05-07 10:19:13 +0800

    加一

--- src/test/java/com/lxr/studydemo/algorithm/easy/ArrayAndSort.java
+++ src/test/java/com/lxr/studydemo/algorithm/easy/ArrayAndSort.java
@@ -486,4 +486,44 @@ public class ArrayAndSort {
         return -1;
     }
 
+    /**
+     * 66. 加一
+     *
+     * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+     *
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     *
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     *
+     * 示例1：
+     *
+     * 输入：digits = [1,2,3]
+     * 输出：[1,2,4]
+     * 解释：输入数组表示数字 123。
+     *
+     * @param digits
+	 * @return int[]
+     */
+    public int[] plusOne(int[] digits) {
+        // 最后一位加一，然后判断是否需要进位
+        int i = digits.length - 1;
+        int temp;
+        int carry = 1; //进位
+        while (i >= 0) {
+            temp = digits[i] + carry;
+            if (temp < 10) {
+                digits[i] = temp;
+                return digits;
+            }
+            carry = temp / 10;
+            digits[i] = temp % 10;
+            i--;
+        }
+        // while跑完说明最后还要进位
+        int[] digitsNew = new int[digits.length + 1];
+        digitsNew[0] = 1;
+        Arrays.copyOfRange(digits, 1, digitsNew.length);
+        return digitsNew;
+    }
+
 }
