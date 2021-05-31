f5251a6b73abad9e04d39a43d2cf274415735f46
刘相然[liuxiangran] <liuxiangran@xiaomi.com>
2021-05-31 10:54:01 +0800

    说明

--- src/test/java/com/lxr/studydemo/git/GitTest.java
+++ src/test/java/com/lxr/studydemo/git/GitTest.java
@@ -3,6 +3,6 @@ package com.lxr.studydemo.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!!!g");
     }
 }
